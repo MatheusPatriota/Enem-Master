5f73b6d00d876ebe22120e1882eac5a0b6c2e43f
MatheusPatriota <mpatriota55@gmail.com>
2023-03-20 21:56:15 -0300

    feat: adding technologies, subjects and subjects cards

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,23 @@
 import Biology from "./assets/biology.svg";
+import Chemistry from "./assets/chemistry.svg";
+import English from "./assets/english.svg";
+import Geography from "./assets/geography.svg";
+import Mathematics from "./assets/mathematics.svg";
+import Spanish from "./assets/spanish.svg";
+import Portuguese from "./assets/portuguese.svg";
+import Philosophy from "./assets/philosopher.svg";
+import Physic from "./assets/physics.svg";
+import Poem from "./assets/poem.svg";
+import Sociology from "./assets/sociology.svg";
+
+import Student from "./assets/student.svg";
 import Nature from "./assets/nature.svg";
 import Languages from "./assets/translation.svg";
 import Science from "./assets/parchment.svg";
 import Math from "./assets/math.svg";
 import Random from "./assets/shuffle.svg";
 import KnowledgeAreaStructure from "./components/KnowledgeAreaStructure";
+import SubjectCard from "./components/SubjectCard";
 
 function Home() {
   const areas = [
@@ -29,15 +42,75 @@ function Home() {
       icon: Random,
     },
   ];
+  // const subjects = [
+  //   "Matemática",
+  //   "Física",
+  //   "Química",
+  //   "Biologia",
+  //   "História",
+  //   "Geografia",
+  //   "Filosofia",
+  //   "Sociologia",
+  //   "Língua Portuguesa",
+  //   "Literatura",
+  //   "Inglês",
+  //   "Espanhol",
+  //   "Educação Física",
+  //   "Artes",
+  // ];
+
+  const subjectAndImages = [
+    { subject: "Matemática", icon: Mathematics },
+    { subject: "Física", icon: Physic },
+    { subject: "Química", icon: Chemistry },
+    { subject: "Biologia", icon: Biology },
+    { subject: "História", icon: Poem },
+    { subject: "Geografia", icon: Geography },
+    { subject: "Filosofia", icon: Philosophy },
+    { subject: "Sociologia", icon: Sociology },
+    { subject: "Língua Portuguesa", icon: Portuguese },
+    { subject: "Literatura", icon: Poem },
+    { subject: "Inglês", icon: English },
+    { subject: "Espanhol", icon: Spanish },
+    { subject: "Educação Física", icon: Poem },
+    { subject: "Artes", icon: Poem },
+  ];
+
+  const years = ["2016", "2017", "2018", "2019", "2020", "2021", "2022"];
+
   return (
     <div className="bg-enem-50 h-full flex flex-col items-center">
-      <div> Capa do Sistema</div>
-      <div>
+      <div className="h-[200px] bg-enem-118ab2 w-[90%] mt-10 mb-6 rounded-lg flex items-center justify-center p-4">
+        <div>
+          <img src={Student} alt="" />
+        </div>{" "}
+        <div className="flex flex-col items-center text-center justify-center text-white font-bold text-[25px] max-w-[200px]">
+          <p>
+            Conquiste sua nota <span className="text-[#08c3a4]">1000</span>
+          </p>
+          <button className="bg-white text-black p-2 rounded-full text-sm mt-2 shadow-lg shadow-black hover:opacity-80">
+            Comece Agora
+          </button>
+        </div>
+      </div>
+      <div className="w-[90%]">
         <div className="flex flex-col flex-wrap">
           <KnowledgeAreaStructure
             title={"Áreas de Conhecimento"}
             areas={areas}
           />
+        </div>
+        <div>
+          {subjectAndImages.map(({ subject, icon }, index) => {
+            return (
+              <SubjectCard
+                title={subject}
+                icon={icon}
+                key={`${subject} - ${index}`}
+                years={years}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
---
+++ src/components/SubjectCard/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+interface SubjectCardProps {
+  icon: any;
+  title: string;
+  years: string[];
+}
+
+function SubjectCard({ title, years, icon }: SubjectCardProps) {
+  return (
+    <>
+      <div className="mt-6 mb-6">
+        <div>
+          <p className="font-bold text-lg mb-4">{title}</p>
+        </div>
+        <div className="flex  gap-4 flex-wrap">
+          {years.map((year, index) => {
+            return (
+              <>
+                <div
+                  key={index}
+                  className="bg-enem-118ab2 p-4 rounded-md flex flex-col min-w-[150px]  min-h-[150px] gap-4 justify-center items-center cursor-pointer hover:opacity-80"
+                >
+                  <div>
+                    <img src={icon} alt="imagem da disciplina" />
+                  </div>
+                  <div className="flex flex-col text-white font-bold">
+                    <p>Enem</p>
+                    <p>{year}</p>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SubjectCard;
