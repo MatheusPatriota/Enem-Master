7b5493847d3e002ef437902aa1c9ba8ca80efe59
MatheusPatriota <mpatriota55@gmail.com>
2023-03-22 21:55:06 -0300

    feat: creating new VideBox style

---
+++ src/components/VideoBox/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function VideoBox() {
+  return (
+    <div className="w-40 h-40 p-4 flex justify-center items-center bg-enem-d8d8de rounded-md">
+      v1
+    </div>
+  );
+}
+
+export default VideoBox;
