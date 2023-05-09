438472046886e6d2e0d3e3d405a6348305762c17
MatheusPatriota <mpatriota55@gmail.com>
2023-05-09 14:45:04 -0300

    feat: score page created

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Question from "./pages/Question";
+import Score from "./pages/Score";
 
 
 const App: React.FC = () => {
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <Routes>
         <Route index path="/" element={<Home/>} />
         <Route path="/question/:id" element={<Question/>} />
+        <Route path="/score" element={<Score/>} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Score.tsx
+++ src/pages/Score.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function Score() {
   return (
-    <div>Score</div>
-  )
+    <>
+      <div className="flex flex-col gap-8 p-10 h-screen">
+        <div className="flex gap-4 items-center justify-between">
+          <div className=" flex items-center gap-8">
+            <div className="text-[30px] font-black">ENEM Master</div>
+          </div>
+        </div>
+        <div className="flex flex-col justify-around items-center h-[100%]">
+          <div className="flex flex-col justify-center items-center">
+            <p className="text-[40px] font-normal">Sua pontuação:</p>
+            <p className="text-[30px] font-black">{100}</p>
+          </div>
+          <button
+            className="bg-enem-75f9a2 hover:opacity-70 text-black font-bold py-2 px-4 rounded w-[400px]"
+            // onClick={handleClose}
+          >
+            Continuar
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Score
+export default Score;
